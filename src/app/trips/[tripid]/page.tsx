db4a03f552809a12a741de972c6eb09c5c9eafea
Joao Souza <joaoosouza07@gmail.com>
2023-07-06 13:31:42 -0300

    :sparkles: feat: Add trip location component

--- src/app/trips/[tripid]/page.tsx
+++ src/app/trips/[tripid]/page.tsx
@@ -5,6 +5,7 @@ import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
 import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
 
 const getTripDetails = async (tripId: string) => {
     const trip = await prisma.trip.findFirst({
@@ -28,6 +29,7 @@ const TripDetails = async ({ params }: { params: { tripId: string }}) => {
             <TripReservation trip={trip} />
             <TripDescription description={trip?.description} />
             <TripHighlights highlights={trip?.highlights} />
+            <TripLocation location={trip?.location} locationDescription={trip?.locationDescription} />
         </div>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
                     <Image width={35} height={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
 
                     {menuIsOpen && (
-                        <div className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+                        <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
                             <button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
                                 Logout
                             </button>
