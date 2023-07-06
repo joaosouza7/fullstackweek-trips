16ae9e9185e49bc28fe23c8f1bdf36453bcbdaef
Joao Souza <joaoosouza07@gmail.com>
2023-07-06 13:30:22 -0300

    :sparkles: feat: Add location description

---
+++ prisma/migrations/20230706162114_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam magni tempora quisquam omnis nisi rerum quaerat cupiditate aut ducimus, quae ut explicabo deleniti nulla reiciendis non doloremque facilis quidem ab!';
