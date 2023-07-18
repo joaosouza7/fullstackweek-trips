c0085516c162032642ccae065aac42236f57ce79
Joao Souza <joaoosouza07@gmail.com>
2023-07-18 20:47:49 -0300

    :sparkles: feat: Add trip buy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,13 +6,24 @@ import { Adapter } from "next-auth/adapters";
 import { prisma } from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    callbacks: {
+        async session({ session, token, user }) {
+            session.user = { ...session.user, id: user.id } as {
+                id: string;
+                name: string;
+                email: string;
+            };
+
+            return session;
+        },
+    },
 };
 
 const handler = NextAuth(authOptions);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,36 @@ import { NextAuthProvider } from "../providers/auth";
 
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
+import ToastProvider from "@/providers/toast";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800", "900"] })
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
-  title: "Trips",
-  description: "Sistema de Reserva de Viagens com Next!",
-}
+    title: "Trips",
+    description: "Sistema de Reserva de Viagens com Next!",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-BR">
-      <body className={poppins.className}>
-        <NextAuthProvider>
-          <Header />
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="pt-BR">
+            <body className={poppins.className}>
+                <NextAuthProvider>
+                    <ToastProvider>
+                        <Header />
 
-          {children}
-          
-          <Footer />
-        </NextAuthProvider>
-      </body>
-    </html>
-  )
+                        {children}
+
+                        <Footer />
+                    </ToastProvider>
+                </NextAuthProvider>
+            </body>
+        </html>
+    );
 }
