902986c59100035082425e43def175c10344ef66
Joao Souza <joaoosouza07@gmail.com>
2023-07-18 20:54:21 -0300

    :sparkles: feat: Add my trips button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,26 @@
 
 import React from "react";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { AiOutlineMenu } from "react-icons/ai";
-
-import { signIn, useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 
 const Header = () => {
-    const { status, data } = useSession();
     const [menuIsOpen, setMenuIsOpen] = React.useState(false);
 
+    const { status, data } = useSession();
+
     const handleLoginClick = () => signIn();
+
     const handleLogoutClick = () => {
         setMenuIsOpen(false);
         signOut();
     };
 
     const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
-    
+
     return (
-        <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
-            
+        <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center lg:border-b lg:border-grayLighter">
             <Link href="/">
                 <div className="relative h-[32px] w-[182px]">
                     <Image src="/logo.png" alt="Full Stack Week" fill />
@@ -29,27 +29,51 @@ const Header = () => {
             </Link>
 
             {status === "unauthenticated" && (
-                <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>
+                <button
+                    className="text-primary text-sm font-semibold"
+                    onClick={handleLoginClick}
+                >
                     Login
                 </button>
             )}
 
             {status === "authenticated" && data.user && (
-                <div className="flex items-center gap-3 border-grayLighter border border-solid p-1 px-3 rounded-full relative">
-                    <AiOutlineMenu size={16} onClick={handleMenuClick} className="cursor-pointer" />
+                <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 px-3 relative">
+                    <AiOutlineMenu
+                        size={16}
+                        onClick={handleMenuClick}
+                        className="cursor-pointer"
+                    />
 
-                    <Image width={35} height={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
+                    <Image
+                        height={35}
+                        width={35}
+                        src={data.user.image!}
+                        alt={data.user.name!}
+                        className="rounded-full shadow-md"
+                    />
 
                     {menuIsOpen && (
-                        <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-                            <button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
+                        <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+                            <Link
+                                href="/my-trips"
+                                onClick={() => setMenuIsOpen(false)}
+                            >
+                                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">
+                                    Minhas Viagens
+                                </button>
+                            </Link>
+
+                            <button
+                                className="text-primary pt-2 text-sm font-semibold"
+                                onClick={handleLogoutClick}
+                            >
                                 Logout
                             </button>
                         </div>
                     )}
                 </div>
             )}
-            
         </div>
     );
 };
