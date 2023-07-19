09e1afb8bdc82cdbf066ea70d1bb255a1021b65a
Joao Souza <joaoosouza07@gmail.com>
2023-07-19 13:47:31 -0300

    :sparkles: feat: Add quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 const QuickSearch = () => {
@@ -6,29 +7,71 @@ const QuickSearch = () => {
         <div className="container mx-auto p-5">
             <div className="flex items-center">
                 <div className="w-full h-[1px] bg-grayLighter"></div>
-                <h2 className="font-medium text-primaryGray whitespace-nowrap px-5">Tente pesquisar por</h2>
+                <h2 className="font-medium text-primaryGray whitespace-nowrap px-5">
+                    Tente pesquisar por
+                </h2>
                 <div className="w-full h-[1px] bg-grayLighter"></div>
             </div>
 
             <div className="flex w-full items-center justify-between mt-5">
                 <div className="flex flex-col items-center gap-1">
-                    <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
-                    <p className="text-sm text-primaryGray">Hotel</p>
+                    <Link
+                        href={`/trips/search?text=hotel`}
+                        className="flex flex-col items-center hover:text-primary transition-all"
+                    >
+                        <Image
+                            width={35}
+                            height={35}
+                            src="/hotel-icon.png"
+                            alt="Hotel"
+                        />
+                        <p className="text-sm text-grayPrimary">Hotel</p>
+                    </Link>
                 </div>
 
                 <div className="flex flex-col items-center gap-1">
-                    <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
-                    <p className="text-sm text-primaryGray">Fazenda</p>
+                    <Link
+                        href="/trips/search?text=fazenda"
+                        className="flex flex-col items-center hover:text-primary transition-all"
+                    >
+                        <Image
+                            width={35}
+                            height={35}
+                            src="/farm-icon.png"
+                            alt="Fazenda"
+                        />
+                        <p className="text-sm text-grayPrimary">Fazenda</p>
+                    </Link>
                 </div>
 
                 <div className="flex flex-col items-center gap-1">
-                    <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
-                    <p className="text-sm text-primaryGray">Chalé</p>
+                    <Link
+                        href="/trips/search?text=Chalé"
+                        className="flex flex-col items-center hover:text-primary transition-all"
+                    >
+                        <Image
+                            width={35}
+                            height={35}
+                            src="/cottage-icon.png"
+                            alt="Chalé"
+                        />
+                        <p className="text-sm text-grayPrimary">Chalé</p>
+                    </Link>
                 </div>
 
                 <div className="flex flex-col items-center gap-1">
-                    <Image width={35} height={35} src="/inn-icon.png" alt="Pousada" />
-                    <p className="text-sm text-primaryGray">Pousada</p>
+                    <Link
+                        href="/trips/search?text=pousada"
+                        className="flex flex-col items-center hover:text-primary transition-all"
+                    >
+                        <Image
+                            width={35}
+                            height={35}
+                            src="/inn-icon.png"
+                            alt="Pousada"
+                        />
+                        <p className="text-sm text-grayPrimary">Pousada</p>
+                    </Link>
                 </div>
             </div>
         </div>
