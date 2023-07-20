5bf8f4b79446ec2d07a6796194c599bbf7f11fdf
Joao Souza <joaoosouza07@gmail.com>
2023-07-19 21:59:32 -0300

    :sparkles: Add tripId files

---
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/Button";
+import Image from "next/image";
+import React from "react";
+
+interface TripLocationProps {
+    location: string;
+    locationDescription: string;
+}
+
+const TripLocation = ({ location, locationDescription }: TripLocationProps) => {
+    return (
+        <div className="p-5">
+            <h2 className="text-primaryDarker font-semibold mb-5">Localização</h2>
+            <div className="relative w-full h-[280px]">
+                <Image 
+                    src="/map-mobile.png" 
+                    alt={location} 
+                    fill 
+                    style={{ objectFit: "cover" }} 
+                    className="rounded-lg shadow-md"
+                />
+            </div>
+
+            <h3 className="text-sm font-semibold text-primaryDarker mt-3">{location}</h3>
+            <p className="text-xs text-primaryDarker mt-2 leading-5">{locationDescription}</p>
+            <Button variant="outlined" className="w-full mt-5">Ver no google maps</Button>
+        </div>
+    );
+};
+
+export default TripLocation;
---
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import ReactCountryFlag from "react-country-flag";
+import ptBR from "date-fns/locale/pt-BR";
+import { useSession } from "next-auth/react";
+import { format } from "date-fns";
+
+import Button from "@/components/Button";
+
+import { Trip } from "@prisma/client";
+import { toast } from "react-toastify";
+import { loadStripe } from "@stripe/stripe-js";
+
+const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
+    const [trip, setTrip] = useState<Trip | null>();
+    const [totalPrice, setTotalPrice] = useState<number>(0);
+
+    const router = useRouter();
+
+    const { status, data } = useSession();
+
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const fetchTrip = async () => {
+            const response = await fetch(`/api/trips/check`, {
+                method: "POST",
+                body: JSON.stringify({
+                    tripId: params.tripId,
+                    startDate: searchParams.get("startDate"),
+                    endDate: searchParams.get("endDate"),
+                }),
+            });
+
+            const res = await response.json();
+
+            if (res?.error) {
+                return router.push("/");
+            }
+
+            setTrip(res.trip);
+            setTotalPrice(res.totalPrice);
+        };
+
+        if (status === "unauthenticated") {
+            router.push("/");
+        }
+
+        fetchTrip();
+    }, [status, searchParams, params, router]);
+
+    if (!trip) return null;
+
+    const handleBuyClick = async () => {
+        const res = await fetch("/api/payment", {
+            method: "POST",
+            body: Buffer.from(
+                JSON.stringify({
+                    tripId: params.tripId,
+                    startDate: searchParams.get("startDate"),
+                    endDate: searchParams.get("endDate"),
+                    guests: Number(searchParams.get("guests")),
+                    totalPrice,
+                    coverImage: trip.coverImage,
+                    name: trip.name,
+                    description: trip.description,
+                })
+            ),
+        });
+
+        if (!res.ok) {
+            return toast.error("Ocorreu um erro ao realizar a reserva!", {
+                position: "bottom-center",
+            });
+        }
+
+        const { sessionId } = await res.json();
+
+        const stripe = await loadStripe(
+            process.env.NEXT_PUBLIC_STRIPE_KEY as string
+        );
+
+        await stripe?.redirectToCheckout({ sessionId });
+
+        toast.success("Reserva realizada com sucesso!", {
+            position: "bottom-center",
+        });
+    };
+
+    const startDate = new Date(searchParams.get("startDate") as string);
+    const endDate = new Date(searchParams.get("endDate") as string);
+    const guests = searchParams.get("guests");
+
+    return (
+        <div className="container mx-auto p-5">
+            <h1 className="font-semibold text-xl text-primaryDarker">
+                Sua viagem
+            </h1>
+
+            {/* CARD */}
+            <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+                <div className="flex items-center gap-3 pb-5 borber-b border-grayLighter border-solid">
+                    <div className="relative h-[106px] w-[124px]">
+                        <Image
+                            src={trip.coverImage}
+                            fill
+                            alt={trip.name}
+                            style={{ objectFit: "cover" }}
+                            className="rounded-sm"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h2 className="text-xl font-semibold text-primaryDarker">
+                            {trip.name}
+                        </h2>
+                        <div className="flex items-center gap-1">
+                            <ReactCountryFlag
+                                countryCode={trip?.countryCode}
+                                svg
+                            />
+                            <p className="text-xs text-primaryGray underline">
+                                {trip?.location}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <h3 className="font-semibold text-lg text-primaryDarker mt-3">
+                    Informações sobre o preço
+                </h3>
+
+                <div className="flex justify-between mt-1">
+                    <p className="text-primaryDarker">Total</p>
+                    <p className="font-medium">R${totalPrice}</p>
+                </div>
+            </div>
+
+            <div className="flex flex-col mt-5 text-primaryDarker">
+                <h3 className="font-semibold">Data</h3>
+                <div className="flex items-center gap-1 mt-1">
+                    <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                    {" - "}
+                    <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                </div>
+
+                <h3 className="font-semibold mt-5">Hóspedes</h3>
+                <p>{guests} hóspedes</p>
+
+                <Button className="mt-5" onClick={handleBuyClick}>
+                    Finalizar compra
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default TripConfirmation;
